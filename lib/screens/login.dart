ead67975d3b23235c61ae9d814221b11a93b2529
momosarhan65 <momosarhan65@gmail.com>
2023-12-07 22:39:02 +0200

    create login screen

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,3 +1,7 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
+import 'package:chatbot/component/loginBottom.dart';
+import 'package:chatbot/component/my_text_filed.dart';
 import 'package:chatbot/screens/homePage.dart';
 import 'package:flutter/material.dart';
 
@@ -9,26 +13,87 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  bool showPassword = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xff6229e8),
       appBar: AppBar(
+        backgroundColor: Color(0xff6229e8),
         title: Text('Login'),
       ),
-      body: Center(
-        child: IconButton(
-          icon: Icon(Icons.login),
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) {
-                  return HomePage();
-                },
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        child: ListView(children: [
+          SizedBox(
+            height: 30,
+          ),
+          Center(
+            child: CircleAvatar(
+              radius: 40,
+              backgroundImage: AssetImage(
+                'assets/images/1.jpg',
               ),
-            );
-          },
-        ),
+            ),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Text(
+            'Welcome Back',
+            style: TextStyle(
+              fontSize: 30,
+            ),
+          ),
+          MyTextFiled(
+              controller: emailController,
+              hintNext: 'Email',
+              obscureText: false),
+          SizedBox(
+            height: 30,
+          ),
+          MyTextFiled(
+            controller: passwordController,
+            hintNext: 'Password',
+            obscureText: true,
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: MyBottom(
+                  ontap: () {},
+                  text: 'Sign In',
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: MyBottom(
+                  ontap: () {
+                    print('pressed');
+                    Navigator.pushReplacement(
+
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) {
+                          return HomePage();
+                        },
+                      ),
+                    );
+                  },
+                  text: 'Login',
+                ),
+              ),
+            ],
+          )
+        ]),
       ),
     );
   }
