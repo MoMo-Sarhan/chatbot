f893758e2b4c99718f094dbf3f943012c32adbf2
momosarhan65 <momosarhan65@gmail.com>
2023-12-26 04:55:03 +0200

    Update UI and add user profile page

--- lib/screens/homePage.dart
+++ lib/screens/homePage.dart
@@ -1,4 +1,4 @@
-//ignore_for_file: prefer_const_constructors
+//ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:chatbot/screens/ChatPage.dart';
 import 'package:chatbot/screens/chatPage2.dart';
@@ -30,103 +30,131 @@ class _HomePageState extends State<HomePage> {
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
   final FirebaseFirestore _firebaseFirestore = FirebaseFirestore.instance;
   ChooseIcon _chooseIcon = ChooseIcon();
+  User? currentUserUid = FirebaseAuth.instance.currentUser;
 
   String? getUserEmail() {
     User? user = FirebaseAuth.instance.currentUser;
     return user!.email;
   }
 
-  UserProfile? userProfile;
-
-  @override
-  void initState() {
-    super.initState();
-    userProfile = UserProfile(
-      username: '${getUserEmail()}',
-      bio: 'Flutter Developer',
-      profilePictureUrl: 'https://example.com/profile_picture.jpg',
-      socialMediaLinks: [
-        'https://twitter.com/johndoe',
-        'https://github.com/johndoe'
-      ],
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-      children: [
-        FutureBuilder(
-            future: _chooseIcon.getUserImageUrl(),
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return CircularProgressIndicator();
-              } else if (snapshot.connectionState == ConnectionState.done &&
-                  snapshot.hasData) {
-                return CircleAvatar(
-                  radius: 70,
-                  backgroundImage: NetworkImage(snapshot.data!),
-                );
-              } else if (snapshot.data == null) {
-                return CircleAvatar(
-                  backgroundImage: AssetImage(
-                    'assets/images/icon.png',
-                  ),
-                );
-              } else {
-                return CircleAvatar(
-                  backgroundImage: AssetImage(
-                    'assets/images/icon.png',
-                  ),
-                );
-              }
-            }),
-        Expanded(child: _buildUserList()),
-      ],
-    ));
+        backgroundColor: Color(0xff6229e8),
+        body: Stack(
+          children: [
+            // Image(
+            //   image: AssetImage('assets/images/hompagebg2.gif'),
+            //   height: MediaQuery.of(context).size.height,
+            //   width: MediaQuery.of(context).size.width,
+            //   fit: BoxFit.fill,
+            // ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                FutureBuilder(
+                    future: _chooseIcon.getUserImageUrl(),
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return CircularProgressIndicator();
+                      } else if (snapshot.connectionState ==
+                              ConnectionState.done &&
+                          snapshot.hasData) {
+                        return CircleAvatar(
+                          radius: 100,
+                          backgroundImage: NetworkImage(snapshot.data!),
+                        );
+                      } else if (snapshot.data == null) {
+                        return CircleAvatar(
+                          backgroundImage: AssetImage(
+                            'assets/images/icon.png',
+                          ),
+                        );
+                      } else {
+                        return CircleAvatar(
+                          backgroundImage: AssetImage(
+                            'assets/images/icon.png',
+                          ),
+                        );
+                      }
+                    }),
+                Expanded(child: _buildUserList()),
+              ],
+            ),
+          ],
+        ));
   }
 
   Widget _buildUserList() {
-    return StreamBuilder<QuerySnapshot>(
-      stream: FirebaseFirestore.instance.collection('users').snapshots(),
-      builder: (context, snapshot) {
-        if (snapshot.hasError) {
-          return const Text('Error');
-        }
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return CircularProgressIndicator();
-        }
-        return ListView(
-          children: snapshot.data!.docs
-              .map<Widget>((doc) => _buildListItem(doc))
-              .toList(),
-        );
-      },
-    );
-  }
-
-  Widget _buildListItem(DocumentSnapshot docs) {
-    Map<String, dynamic> data = docs.data()! as Map<String, dynamic>;
-    if (_firebaseAuth.currentUser!.email != data['email']) {
-      return ListTile(
-        title: Text(
-          data['email'],
-          style: TextStyle(color: Colors.black),
-        ),
-        onTap: () {
-          Navigator.push(context, MaterialPageRoute(builder: (context) {
-            return ChatPage2(
-              reciverId: data['uid'],
-              reciverEmail: data['email'],
+    return FutureBuilder(
+        future: FirebaseFirestore.instance
+            .collection('users')
+            .doc(currentUserUid!.uid)
+            .get(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Center(
+              child: Text('Error'),
             );
-          }));
-        },
-      );
-    } else {
-      return Container(
-        child: Text('me'),
-      );
-    }
+          }
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          Map<String, dynamic> data = snapshot.data!.data()!;
+          return ListView(
+            children: [
+              ListTile(
+                leading: Icon(
+                  Icons.person,
+                  color: Colors.blue,
+                  size: 50,
+                ),
+                title: Text(
+                  data['userName'],
+                  style: TextStyle(
+                      color: Colors.amber,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20),
+                ),
+              ),
+              ListTile(
+                leading: data['gender'] == 'male'
+                    ? Icon(
+                        Icons.male,
+                        color: Colors.blue,
+                        size: 50,
+                      )
+                    : Icon(
+                        Icons.female,
+                        color: Colors.blue,
+                        size: 50,
+                      ),
+                title: Text(
+                  data['gender'],
+                  style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.amber,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+              ListTile(
+                leading: Icon(
+                  Icons.email,
+                  color: Colors.blue,
+                  size: 50,
+                ),
+                title: Text(
+                  data['email'],
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.amber),
+                ),
+              ),
+            ],
+          );
+        });
   }
 }
